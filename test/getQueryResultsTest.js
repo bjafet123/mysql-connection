3d73c7c3bf748c3b63363f3416acb33d0249a120
bjafet123 <bahamut.jafet@gmail.com>
2022-07-08 18:28:06 -0500

    rabbitmq configuration updated

--- test/getQueryResultsTest.js
+++ test/getQueryResultsTest.js
@@ -1,66 +1,85 @@
 
 require('dotenv').config();
-const express = require('express');
 const mysql = require('mysql');
+const express = require('express');
 const app = express();
 const {readPool} = require('./../helpers/query')
+const log = require('../helpers/logger');
+const rabbitmq = require('../helpers/rabbit');
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get("/",(req, res) => {
-	res.send("Service ready.");
-});
-
 app.post("/", async (req, res) => {
 	try {		 
-		const {
-				query, 
-				host,
-				user,
-				password,
-				database,
-				port
-			} = req.body;
 		
-		if (!host) {
-			return res.status(400).json({msg: 'Database host parameter missing.'});
-		}
-		if (!user) {
-			return res.status(400).json({msg: 'Database user name parameter missing.'});
-		}
-		if (!password) {
-			return res.status(400).json({msg: 'Database password parameter missing.'});
-		}
-		if (!database) {
-			return res.status(400).json({msg: 'Database name parameter missing.'});
-		}
-		if (!port) {
-			return res.status(400).json({msg: 'Database port parameter missing.'});
-		}
+		const {data} = req.body;
+        const {cfg} = req.body;
+        let snapshot = {};
+		
+		let properties = {
+            database: null,
+            host: null,
+            port: null,
+            user: null,
+            password: null,
+            query: null
+        };
+		
+		if (!data) {
+            res.status(401).json('Error missing data property');
+            return;
+        }
+        if (!cfg) {
+            res.status(401).json('Error missing cfg property');
+            return;
+        }
+        
+        Object.keys(properties).forEach((value) => {
+            if (data.hasOwnProperty(value)) {
+                properties[value] = data[value];
+            } else if (cfg.hasOwnProperty(value)) {
+                properties[value] = cfg[value];
+            } else {
+                log.error('ERROR missing property', value);
+                throw new Error(`ERROR missing property, ${value}`);
+            }
+        });
+        
 		const pool = mysql.createPool({
-			host,
-			user,
-			password,
-			database,
-			port
+			host : properties.host,
+			user : properties.user,
+			password : properties.password,
+			database : properties.database,
+			port : properties.port
 		});
+		log.info('Connection pool started...');
 		
-		if (!query) {
-			return res.status(400).json({msg: 'No query to be excecuted.'});
-		}
-		//console.log(query);
-		readPool (pool, query, (result) => {
-			res.json(result);
-		});
-	} catch (err) {
-		res.status(400).json(err);
+		snapshot.lastUpdated = snapshot.lastUpdated || new Date();
+		
+		const _data = await readPool(pool, properties.query);
+		log.info("data", _data);
+		
+		if (_data.length > 0) {
+			log.info("data", _data);
+            res.json(_data);
+        } else {
+			log.info("data", "Query successfully executed");
+			res.json("Query successfully executed");
+        }
+        
+        snapshot.lastUpdated = new Date();
+        log.info(`New snapshot: ${snapshot.lastUpdated}`);
+		
+		log.info('Finished execution');
+        log.info('end');
+	} catch (e) {
+		log.error(`ERROR: ${e}`);
+        await rabbitmq.producerMessage(e);
+        res.status(500).json(e);
 	}
 });
 
-app.listen(PORT, () => {
-	console.log("Server running on port 3000...");
-});
-
+app.listen(PORT, () => log.info('Service up in port', PORT));
 
