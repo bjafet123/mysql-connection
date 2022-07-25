7d0448351918c9d724d587d4d40e828681736ea3
a <email@email.com>
2022-07-25 11:48:57 -0500

    new way to connect rabbitmq, optimize code

--- helpers/query.js
+++ helpers/query.js
@@ -1,26 +1,18 @@
 const mysql = require('mysql');
 const log = require('./logger');
 
-function read (connection, query, callback) {
-	connection.query(query, function (err, result) {
-		if (err) throw new Error('No connection established.');
-		callback(result);
-		connection.end();
-	});
-}
-
-async function readPool (pool, query) {
-	return new Promise((resolve, reject) => {
-		pool.getConnection(function (err, connection) {
-			if (err) reject(err);
-			connection.query(query, function (err, result) {
-				if (err) reject(err);
-				resolve(result);
-				connection.release();
-			});
-			log.info("Executed");
-		});
-	});
+async function readPool(pool, query) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection(function (err, connection) {
+            if (err) reject(err);
+            connection.query(query, function (err, result) {
+                if (err) reject(err);
+                resolve(result);
+                connection.release();
+            });
+            log.info('Executed');
+        });
+    });
 }
 
 module.exports = {readPool}
--- test/getQueryResultsTest.js
+++ test/getQueryResultsTest.js
@@ -1,24 +1,23 @@
-
 require('dotenv').config();
 const mysql = require('mysql');
 const express = require('express');
 const app = express();
 const {readPool} = require('./../helpers/query')
 const log = require('../helpers/logger');
-const rabbitmq = require('../helpers/rabbit');
+const rabbitmq = require('rabbitmqcg-nxg-oih');
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.post("/", async (req, res) => {
-	try {		 
-		
-		const {data} = req.body;
+app.post('/', async (req, res) => {
+    try {
+
+        const {data} = req.body;
         const {cfg} = req.body;
         let snapshot = {};
-		
-		let properties = {
+
+        let properties = {
             database: null,
             host: null,
             port: null,
@@ -26,8 +25,8 @@ app.post("/", async (req, res) => {
             password: null,
             query: null
         };
-		
-		if (!data) {
+
+        if (!data) {
             res.status(401).json('Error missing data property');
             return;
         }
@@ -35,7 +34,7 @@ app.post("/", async (req, res) => {
             res.status(401).json('Error missing cfg property');
             return;
         }
-        
+
         Object.keys(properties).forEach((value) => {
             if (data.hasOwnProperty(value)) {
                 properties[value] = data[value];
@@ -46,40 +45,39 @@ app.post("/", async (req, res) => {
                 throw new Error(`ERROR missing property, ${value}`);
             }
         });
-        
-		const pool = mysql.createPool({
-			host : properties.host,
-			user : properties.user,
-			password : properties.password,
-			database : properties.database,
-			port : properties.port
-		});
-		log.info('Connection pool started...');
-		
-		snapshot.lastUpdated = snapshot.lastUpdated || new Date();
-		
-		const _data = await readPool(pool, properties.query);
-		log.info("data", _data);
-		
-		if (_data.length > 0) {
-			log.info("data", _data);
+
+        const pool = mysql.createPool({
+            host: properties.host,
+            user: properties.user,
+            password: properties.password,
+            database: properties.database,
+            port: properties.port
+        });
+        log.info('Connection pool started...');
+
+        snapshot.lastUpdated = snapshot.lastUpdated || new Date();
+
+        const _data = await readPool(pool, properties.query);
+        log.info('data', _data);
+
+        if (_data.length > 0) {
+            log.info('data', _data);
             res.json(_data);
         } else {
-			log.info("data", "Query successfully executed");
-			res.json("Query successfully executed");
+            log.info('data', 'Query successfully executed');
+            res.json('Query successfully executed');
         }
-        
+
         snapshot.lastUpdated = new Date();
         log.info(`New snapshot: ${snapshot.lastUpdated}`);
-		
-		log.info('Finished execution');
+
+        log.info('Finished execution');
         log.info('end');
-	} catch (e) {
-		log.error(`ERROR: ${e}`);
+    } catch (e) {
+        log.error(`ERROR: ${e}`);
         await rabbitmq.producerMessage(e);
         res.status(500).json(e);
-	}
+    }
 });
 
 app.listen(PORT, () => log.info('Service up in port', PORT));
-
